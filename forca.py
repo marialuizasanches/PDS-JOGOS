681f8b18cf69bb709e2692733e984afa656f82b0
Maria Luiza Sanches <130071736+marialuizasanches@users.noreply.github.com>
2024-03-26 09:25:11 -0300

    forca aula 1

---
+++ forca.py
@@ -0,0 +1,31 @@
+
+def jogar():
+print("*****
+print("***Maria Luiza Sanches***")
+palavra secreta = "Maria Luiza".upper()
+letras_acertadas = ["_" for letra in palavra secreta]
+letras acertadas2 = []
+for letra in (palavra secreta):
+letras acertadas2.append("_")
+enforcou = False
+acertou False
+erros = 0
+print(letras acertadas)
+while (not enforcou and not acertou):
+chute = input("Qual letra? ")
+chute chute.strip().upper()
+if (chute in palavra secreta): index = 0 for letra in palavra_secreta:
+if (chute == letra):
+letras acertadas [index] = letra
+index += 1
+else:
+erros += 1
+I
+enforcou erros == 6 acertou = "_" not in letras acertadas www
+print(letras_acertadas)
+if (acertou):
+print("Você ganhou!!")
+else: print("Você perdeu!!")
+print("Fim do jogo")
+if (_name__ == "__main__"):
+jogar()
